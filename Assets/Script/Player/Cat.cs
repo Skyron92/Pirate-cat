f3ab8ca23f162c4a06ea75566b6cdfbd681a122b
Skyron92 <courtier.leo@gmail.com>
2023-03-07 18:31:17 +0100

    Personalisation

--- Assets/Script/Player/Cat.cs
+++ Assets/Script/Player/Cat.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Cat : MonoBehaviour {
     public int NavigateurStat;
     public int CanonnierStat;
     public int EscrimeurStat;
+    [HideInInspector] public GameObject Hat;
+    [HideInInspector] public GameObject EyePatch;
+    [HideInInspector] public Fur FurCat;
+    [HideInInspector] public Material Clothes;
+    [SerializeField] private MeshRenderer Body;
+    [SerializeField] private MeshRenderer Face;
+    [SerializeField] private MeshRenderer Ear;
+    [SerializeField] private MeshRenderer LeftHand;
+    [SerializeField] private MeshRenderer RightHand;
+    [SerializeField] private MeshRenderer Tail;
 
-    public Cat(int navigateurStat, int canonnierStat, int escrimeurStat) {
-        NavigateurStat = navigateurStat;
-        CanonnierStat = canonnierStat;
-        EscrimeurStat = escrimeurStat;
+    private void Update() {
+        SwitchSkin();
+    }
+
+    void SwitchSkin() {
+        Body.material = Clothes;
+        Face.material = FurCat.Face;
+        Ear.material = FurCat.Ear;
+        LeftHand.material = FurCat.LeftHand;
+        RightHand.material = FurCat.RightHand;
+        Tail.material = FurCat.Tail;
     }
 }
---
+++ Assets/Script/Player/CatEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEditor : MonoBehaviour
+{
+    [SerializeField] private Cat cat;
+    [SerializeField] private List<GameObject> hats = new List<GameObject>();
+    [SerializeField] private List<GameObject> eyePatches = new List<GameObject>();
+    [SerializeField] private List<Material> clothes = new List<Material>();
+    [SerializeField] private List<Fur> furs = new List<Fur>();
+    private int hatIndex;
+    private int eyePatchesIndex;
+    private int clothesIndex;
+    private int furIndex;
+
+    public void SwitchHat(int ChangeValue) {
+        hatIndex += ChangeValue;
+        if (hatIndex >= hats.Count) hatIndex = 0;
+        if (hatIndex < 0) hatIndex = hats.Count - 1;
+        cat.Hat = hats[hatIndex];
+    }
+    
+    public void SwitchClothes(int ChangeValue) {
+        clothesIndex += ChangeValue;
+        if (clothesIndex >= clothes.Count) clothesIndex = 0;
+        if (clothesIndex < 0) clothesIndex = clothes.Count - 1;
+        cat.Clothes = clothes[clothesIndex];
+    }
+    
+    public void SwitchFur(int ChangeValue) {
+        furIndex += ChangeValue;
+        if (furIndex >= furs.Count) furIndex = 0;
+        if (furIndex < 0) furIndex = furs.Count - 1;
+        cat.FurCat = furs[furIndex];
+    }
+    
+    public void SwitchEyePatch(int ChangeValue) {
+        eyePatchesIndex += ChangeValue;
+        if (eyePatchesIndex >= eyePatches.Count) eyePatchesIndex = 0;
+        if (eyePatchesIndex < 0) eyePatchesIndex = eyePatches.Count - 1;
+        cat.EyePatch = eyePatches[eyePatchesIndex];
+    }
+
+}
